21746b96a0bed4f0ed7e014aeea0016b421c168c
UjjawalM <ujjawalmodamwal352@gmail.com>
2026-07-15 21:50:27 +0530

    remove swagger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
 import { materialRoutes } from './routes/materialRoutes.js';
-import { swaggerSpec } from './swagger.js';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-// API Documentation
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 app.use('/api/materials', materialRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
